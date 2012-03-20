049a6acf215f3672b4b513e61170e0759bb8490a
Fabio Fabbrucci <fabio.fabbrucci@gmail.com>
2012-03-20 16:15:17 +0100

    Capifony recipe completed

--- recipes/database.rb
+++ recipes/database.rb
@@ -16,6 +16,32 @@ namespace :db do
   desc "Download file in local folder" 
   task :download, :roles => :db, :only => { :primary => true } do 
     #param = IniFile::load('app/config/parameters.ini')
+    system "rm app/tmp/dump/remote.dbcurrent.sql*"
     run "cp #{latest_release}/app/tmp/dump/current.sql.bz2 #{latest_release}/web/dbcurrent.sql.bz2"
+    get "#{latest_release}/web/dbcurrent.sql.bz2", "app/tmp/dump/remote.dbcurrent.sql.bz2"
+    run "rm #{latest_release}/web/dbcurrent.sql.bz2"
+  end
+
+  namespace :import do
+
+    desc "Import remote db in local production database" 
+    task :production, :roles => :db, :only => { :primary => true } do 
+        set :local_db_name, "abmundi"
+        db.download
+        param = IniFile::load('app/config/parameters.ini')['parameters']
+        system "bunzip2 -f app/tmp/dump/remote.dbcurrent.sql.bz2"
+        system "mysql -u #{param['database_user']} --password=#{param['database_password']} #{local_db_name} < app/tmp/dump/remote.dbcurrent.sql"
+        puts "Import complete"
+    end
+
+    desc "Import remote db in local testing database" 
+    task :testing, :roles => :db, :only => { :primary => true } do 
+        set :local_db_name, "abmundi_test"
+        db.download
+        param = IniFile::load('app/config/parameters.ini')['parameters']
+        system "bunzip2 -f app/tmp/dump/remote.dbcurrent.sql.bz2"
+        system "mysql -u #{param['database_user']} --password=#{param['database_password']} #{local_db_name} < app/tmp/dump/remote.dbcurrent.sql"
+        puts "Import complete"
+    end
   end
 end
